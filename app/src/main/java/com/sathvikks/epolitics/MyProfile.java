f259418811ad59d17c614bc0b1001cedfc87e90c
Sathvik K S <sathvikks8@gmail.com>
2022-07-05 20:25:16 +0530

    added account delete functionality

--- app/src/main/java/com/sathvikks/epolitics/MyProfile.java
+++ app/src/main/java/com/sathvikks/epolitics/MyProfile.java
@@ -3,6 +3,7 @@ package com.sathvikks.epolitics;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,10 +17,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,9 +32,12 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -42,6 +50,7 @@ import java.util.List;
 import java.util.Objects;
 
 public class MyProfile extends AppCompatActivity {
+    String userName, userEmail, userPhone, userRegion, userType, userGender, userPic, reEnterPassword;
     ProgressDialog dialog;
     Bitmap selectedImageBitmap;
     ImageView profileImage;
@@ -109,7 +118,6 @@ public class MyProfile extends AppCompatActivity {
             }
         });
         userObj = Configs.fetchUserInfo(MyProfile.this, false);
-        String userName, userEmail, userPhone, userRegion, userType, userGender, userPic;
         try {
             userName = (String) userObj.get("name");
             userEmail = (String) userObj.get("email");
@@ -189,6 +197,88 @@ public class MyProfile extends AppCompatActivity {
                         }
                     });
                 }
+            }
+        });
+        profileUserDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MyProfile.this)
+                        .setTitle("Confirm Delete")
+                        .setMessage("Are you sure you want to delete your account?")
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                
+                            }
+                        })
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialog = Configs.showProcessDialogue(MyProfile.this, "Re Authenticating...");
+                                ContextThemeWrapper ctx = new ContextThemeWrapper(MyProfile.this, com.google.android.material.R.style.Theme_MaterialComponents_DayNight_DarkActionBar);
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MyProfile.this);
+                                builder.setTitle("Re enter the account password");
+                                View viewInflated = LayoutInflater.from(ctx).inflate(R.layout.text_input_password, null);
+                                final EditText input = (EditText) viewInflated.findViewById(R.id.input);
+                                builder.setView(viewInflated);
+                                builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogg, int which) {
+                                        dialog.show();
+                                        reEnterPassword = input.getText().toString();
+                                        AuthCredential credential = EmailAuthProvider.getCredential(userEmail, reEnterPassword);
+                                        mAuth.getCurrentUser().reauthenticate(credential)
+                                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        if (task.isSuccessful()) {
+                                                            Log.i("sksLog", "User re-authenticated.");
+                                                            mAuth.getCurrentUser().delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                                @Override
+                                                                public void onComplete(@NonNull Task<Void> task) {
+                                                                    if (!task.isSuccessful()){
+                                                                        Toast.makeText(MyProfile.this, "Unable to delete your account at the moment. Please try again later.", Toast.LENGTH_LONG).show();
+                                                                        Log.e("sksLog", "user.delete() error: "+task.getException().toString());
+                                                                        return;
+                                                                    }
+                                                                    dbRef.child("users").child(Configs.generateEmail(userEmail)).removeValue(new DatabaseReference.CompletionListener() {
+                                                                        @Override
+                                                                        public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                                                                            if(error!=null) {
+                                                                                Toast.makeText(MyProfile.this, "Unable to delete your account at the moment. Please try again later.", Toast.LENGTH_LONG).show();
+                                                                                Log.e("sksLog", "dbref.removeValure() error: "+error.toString());
+                                                                                return;
+                                                                            }
+                                                                            Toast.makeText(MyProfile.this, "Your account has been deleted", Toast.LENGTH_SHORT).show();
+                                                                            startActivity(siIntent);
+                                                                            finish();
+                                                                        }
+                                                                    });
+                                                                }
+                                                            });
+                                                        } else {
+                                                            Log.e("sksLog", "unable to reauthenticate "+task.getException().toString());
+                                                            Toast.makeText(MyProfile.this, "Wrong password", Toast.LENGTH_SHORT).show();
+                                                        }
+                                                        dialog.dismiss();
+                                                    }
+                                                });
+
+                                    }
+                                });
+                                builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                                builder.show();
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .show();
+                
             }
         });
     }
